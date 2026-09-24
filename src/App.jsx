bfff2684234d61951ae3ff050bcf9fe2a405bbd5
Hidemi2008 <Gabrielhidemi2@gmail.com>
2026-09-24 13:23:44 -0400

    Criado template básico

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+const URL = "https://jsonplaceholder.typicode.com"
+
+export default function App(){
+
+
+  return (
+    <body>
+      <header>
+        <h1>Banco de Ideias</h1>
+        <p>Projeto P1 - PTAC4 . anotacao de ideias de projetos</p>
+      </header>
+    </body>
+  )
+}
