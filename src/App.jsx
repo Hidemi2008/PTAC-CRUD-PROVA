b4f3a8779a14fba800f8e1aa45ebff6d1b5138b5
Hidemi2008 <Gabrielhidemi2@gmail.com>
2026-09-24 13:50:46 -0400

    Alterado os components para dentro do app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,66 @@
 import { useState, useEffect } from "react";
-import FormularioIdeia from "./components/FormularioIdeia";
-import ListarIdeias from "./components/ListaIdeias";
 
-const URL = "https://jsonplaceholder.typicode.com"
+const URL = "https://jsonplaceholder.typicode.com/todos"
 
 export default function App() {
+  const [ideias, setIdeias] = useState([])
+  const [carregando, setCarregando] = useState(true)
+  const [erro, setErro] = useState(null)
 
+  useEffect(() => {
+    async function buscar() {
+      try {
+        setCarregando(true)
+        setErro(null)
+        const resp = await fetch(URL)
+        if (!resp.ok) {
+          // 4xx ou 5xx — fetch NÃO rejeita para esses status! Precisamos lançar à mão.
+          throw new Error(`HTTP ${resp.status} — ${resp.statusText}`)
+        }
+        const data = await resp.json()
+        setIdeias(data)
+        console.log(ideias)
+      } catch (e) {
+        setErro(e.message)
+      } finally {
+        setCarregando(false)
+      }
+    }
+
+    buscar()
+  }, [])
+
+  if (carregando) return <p>Carregando...</p>
+  if (erro) return <p>Erro: {erro}</p>
+  if (ideias.length === 0) return <p>Nenhum usuário encontrado.</p>
 
   return (
-    <body>
+    <>
       <header>
         <h1>Banco de Ideias</h1>
         <p>Projeto P1 - PTAC4 . anotacao de ideias de projetos</p>
       </header>
 
-      <FormularioIdeia></FormularioIdeia>
+      <div>
+        <form action="">
+          <h1>Nova Ideia</h1>
+          <p>Título</p>
+          <input type="text" placeholder="teste" />
+          <button>Adicionar ideia</button>
+        </form>
+      </div>
 
-      <ListarIdeias></ListarIdeias>
-    </body>
+      <section>
+        <h1>Minhas ideias</h1>
+
+        <div>
+          <h1>{ideias.title}</h1>
+          <p>{ideias.completed}</p>
+          <button>Marca executada</button>
+          <button>Editar</button>
+          <button>Excluir</button>
+        </div>
+      </section>
+    </>
   )
 }
--- src/components/FormularioIdeia.jsx
+++ src/components/FormularioIdeia.jsx
@@ -5,13 +5,6 @@ const URL = "https://jsonplaceholder.typicode.com"
 export default function FormularioIdeia(){
 
   return (
-    <div>
-        <form action="">
-            <h1>Nova Ideia</h1>
-            <p>Título</p>
-            <input type="text" placeholder="teste"/>
-            <button>Adicionar ideia</button>
-        </form>
-    </div>
+
   )
 }
